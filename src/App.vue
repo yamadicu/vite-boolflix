6708e7fa0e0606ec448d7ea1a9fae85ef8cb6a5e
yamadicu <dicutia@gmail.com>
2023-05-10 16:05:02 +0200

    fine navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <script>
+import NavbarComp from './componets/NavbarComp.vue';
+import axios from 'axios';
+import { store } from './store';
+
+export default {
+    name: "app",
+    components: {
+        NavbarComp
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        AttivaRicerca() {
+            // 
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${store.Apikey}&query=${store.Ricerca}`)
+                .then((elem) => {
+                    console.log(elem.data.results)
+                    const selezione = elem.data.results
+                    store.ArrayFilm = selezione
+
+                })
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <NavbarComp @apiCall="AttivaRicerca()" />
+</template>
 
 <style lang="scss">
 @use './style/main.scss';
---
+++ src/componets/NavbarComp.vue
@@ -0,0 +1,35 @@
+<script>
+import { store } from '../store'
+export default {
+    name: "NavbarComp",
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <nav class="container">
+
+        <span id="app-title" class="fw-bold fs-1 text-uppercase">boolflix</span>
+        <div>
+            <input type="text" placeholder="ricerca film o serie" v-model="store.Ricerca">
+            <button @click.prevent="$emit('apiCall')" class="">Invio</button>
+        </div>
+
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    #app-title {
+        color: rgb(129, 23, 23);
+    }
+}
+</style>
